3c5a4de223e9cdf3ecc4f20e0222ba12097489c9
Rafael Affonso <hrafaelaf@gmail.com>
2021-09-21 17:08:38 -0300

    altera script para separar ação em função

--- git_tag_versioning.sh
+++ git_tag_versioning.sh
@@ -29,19 +29,9 @@ case "$type" in
     *) printf '\nError: invalid type "%s"\n' "$type"; helpFunction;;
 esac
 
-echo "Prefix: $prefix";
-echo "Semver type: $type";
-if [ -n "$force" ]; then
-    echo "Forcing execution. I sure hope you know what you are doing."
-fi
-
-# Initializes flags and other variables
-prefix="$prefix-"
-versionFile=false   # Flag to generate the .semver file
-last=''             # Will contain the last version tag found
 
 ## Runs a command with arguments, taking care of the --dry flag
-function execute {
+function runCommand {
     cmd=("$@")
 
     printf '%s\n' "Executing ${cmd[*]} ..."
@@ -59,35 +49,19 @@ function confirm() {
 }
 
 
-# Shows the result of the command
-function showSuccess() {
-    echo
-    echo "Tag {$prefix}v$major.$minor.$patch was created in local repository."
-    echo
-    echo "Push it:"
-    echo
-    echo "    git push origin {$prefix}v$major.$minor.$patch"
-    echo
-    echo "Delete tag:"
-    echo
-    echo "    git tag -d {$prefix}v$major.$minor.$patch"
-    echo
-}
-
-
 # Get the last tag and breaks into parts
-function getLastTag() {
+function getLastTag {
+    env=$1
     #get all tags from remote
     t=`git fetch --tags -f`
-    lasttag=`git tag | sort -r --version-sort | grep "^${prefix}v[0-9]*\.[0-9]*\.[0-9]*" | head -1 2> /dev/null` || true
+    lasttag=`git tag | sort -r --version-sort | grep "^${env}-v[0-9]*\.[0-9]*\.[0-9]*" | head -1 2> /dev/null` || true
 
     if [[ -z "$lasttag" ]]; then
         lasttag="v0.0.0"
     fi
 
-    lasttag=${lasttag:0}
-
     parts=(${lasttag//./ })
+    parts=${parts#"$env-v"}
 
     major=${parts[0]}
     minor=${parts[1]}
@@ -96,51 +70,58 @@ function getLastTag() {
     last="$major.$minor.$patch"
 }
 
-# Get last tag from repo and process the desired command
-getLastTag
+function generateTag {
+    env=$1
+    echo "Prefix: $env";
+    echo "Semver type: $type";
+    if [ -n "$force" ]; then
+        echo "Forcing execution. I sure hope you know what you are doing."
+    fi
 
-case "$type" in
-    'patch')
-        patch=$((patch + 1))
-        ;;
-    'minor')
-        minor=$((minor + 1))
-        patch=0
-        ;;
-    'major')
-        major=${prefix}v$((major + 1))
-        minor=0
-        patch=0
-        ;;
-    *)
-        echo "No mode selected"
-        exit 0
-esac
+    
+    last='' # Will contain the last version tag found
+    # Get last tag from repo and process the desired command
+    getLastTag $env
 
-# Shows information
-printf "Current version : %s\n" "$last"
-printf "New tag         : %s.%s.%s\n\n" "$major" "$minor" "$patch"
+    case "$type" in
+        'patch')
+            patch=$((patch + 1))
+            ;;
+        'minor')
+            minor=$((minor + 1))
+            patch=0
+            ;;
+        'major')
+            major="$((major +1))"
+            minor=0
+            patch=0
+            ;;
+        *)
+            echo "No mode selected"
+            exit 0
+    esac
 
-# Forces tag push if --force option is set to "true"
-if [ -n "$force" ]; then
-    echo "execute git tag $major.$minor.$patch"
-    echo "execute git push origin $major.$minor.$patch"
-fi
+    # Shows information
+    printf "Current version : $env-v%s\n" "$last"
+    printf "New tag         : ${env}-v%s.%s.%s\n\n" "$major" "$minor" "$patch"
 
-# Creates local tag
-if [[ ! -n "$force" && "no" == $(confirm "Do you agree?") ]]; then
-    echo "No tag was created"
-    exit 0;
-fi
+    # Creates local tag
+    if [[ ! -n "$force" && "no" == $(confirm "Do you agree?") ]]; then
+        echo "No tag was created"
+        exit 0;
+    fi
 
-execute git tag "$major.$minor.$patch"
+    runCommand git tag "${env}-v$major.$minor.$patch"
 
-# Pushes tag
-if [[ ! -n "$force" && "no" == $(confirm "Do you want to push this tag right now?") ]]; then
-    echo "No tag was pushed"
-    exit 0;
-fi
+    # Pushes tag
+    if [[ ! -n "$force" && "no" == $(confirm "Do you want to push this tag right now?") ]]; then
+        echo "No tag was pushed"
+        exit 0;
+    fi
 
-execute git push origin "$major.$minor.$patch"
+    runCommand git push origin "${env}-v$major.$minor.$patch"
+}
+
+generateTag $prefix
 
 exit 0
